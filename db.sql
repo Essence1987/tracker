a340322b0aff826d23b86027d10502915d8da6d8
Essence1987 <hwmelander@gmail.com>
2023-07-12 17:35:46 -0600

    Sending up a temporary db.sql for development need

---
+++ db.sql
@@ -0,0 +1,9 @@
+-- Creates the departments table
+CREATE TABLE departments (
+    id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+-- Inserts sample data into the departments table for testing purposes
+INSERT INTO departments (id, name) VALUES (1, 'Department A');
+INSERT INTO departments (id, name) VALUES (2, 'Department B');
