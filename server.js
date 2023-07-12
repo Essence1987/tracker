a0c72d2164eefb8a5bed934e3f7c2a296ad4654b
Essence1987 <hwmelander@gmail.com>
2023-07-12 17:32:15 -0600

    Set up basic Inquirer prompts for options

---
+++ server.js
@@ -0,0 +1,64 @@
+const inquirer = require('inquirer');
+
+// Starts the Application
+function startApplication() {
+    console.log('Welcome to the Employee Tracking Database!');
+    showOptions();
+}
+
+// Calls the startApplication function to begin the program.
+startApplication();
+
+// Database Options
+function showOptions() {
+    console.log ('Please select an option by entering the corresponding number:\n');
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'Choose an option:',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ],
+        },
+    ])
+    .then((answers) => {
+        // Handles the selected option
+        handleOption(answers.option);
+    });
+}
+
+// Helper function to handle the selected option
+function handleOption(option) {
+    switch (option) {
+        case 'View all departments':
+            viewAllDepartments();
+            break;
+        case 'View all roles':
+            viewAllRoles();
+            break;
+        case 'View all employees':
+            viewAllEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        default:
+            console.log('Invalid option. Please choose a valid option.\n');
+    }
+}
+
+// call the showOptions function to display the available options
+showOptions();
